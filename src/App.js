261982e6741af16c21e0075142414a1a5445eb09
are305 <are305@ensign.edu>
2024-08-26 19:56:56 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,6 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 // import LandingPage from "./components/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
-function NoMatch() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>404: Page Not Found</h2>
-      <p><font color="white">There is no page at the requested url</font></p>
-    </div>
-  );
-}
-
-function UnAuthorized() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>401: Unauthorized Test</h2>
-      <p><font color="white">You are not authorized to view the page at the requested url</font></p>
-    </div>
-  );
-}
-
 function App() {
   return (
     <Router>
