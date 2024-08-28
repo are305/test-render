7bcf947a218f09082ee9b0f0c26786c0d99954f0
are305 <are305@ensign.edu>
2024-08-28 08:18:23 -0600

    beta version

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,5 @@
+export function formatDateString(dateString) {
+    const date = new Date(dateString);
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+}
